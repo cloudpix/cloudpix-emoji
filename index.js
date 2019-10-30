3ada17da17f05c5c65d8322845b462e2bc523b3d
Rafael Cruz <rafael.cruz@iscoolapp.com.br>
2019-10-30 11:31:46 -0300

    grouped by emoji sources.

--- index.js
+++ index.js
@@ -1,49 +1,121 @@
 'use strict';
 
-const notoEmojiSVGs = require.context('../noto-emoji/svg/', true, /^\.\/.*\.(svg)$/i);
-const twemojiSVGs = require.context('../twemoji/assets/svg/', true, /^\.\/.*\.(svg)$/i);
+const isDev = (process.env.NODE_ENV || 'dev').startsWith('dev');
+
+const notoEmojiSVGs = (() => {
+  try {
+    return require.context('../noto-emoji/svg/', true, /^\.\/.*\.(svg)$/i);
+  } catch (e) {
+    isDev && console.error(e);
+    return code => null;
+  }
+})();
+
+const twemojiSVGs = (() => {
+  try {
+    return require.context('../twemoji/assets/svg/', true, /^\.\/.*\.(svg)$/i);
+  } catch (e) {
+    isDev && console.error(e);
+    return code => null;
+  }
+})();
 
 const emojis = require('./data/emoji.json');
+const categories = [];
+
+function getFromNotoEmoji(code) {
+  try {
+    return notoEmojiSVGs(`./emoji_u${(code || '').toLowerCase()}.svg`);
+  } catch (e) {
+    isDev && console.warn(`Couldn't find SVG in Noto for code: ${code}`);
+    return null;
+  }
+}
+
+function getFromTwemoji(code) {
+  try {
+    return twemojiSVGs(`./${(code || '').replace(/_/g, '-').toLowerCase()}.svg`);
+  } catch (e) {
+    isDev && console.warn(`Couldn't find SVG in Twemoji for code: ${code}`);
+    return null;
+  }
+}
 
 function getEmoji(code) {
-  let svg;
-  try {
-    svg = notoEmojiSVGs('./emoji_u' + (code || '').toLowerCase() + '.svg');
-  } catch (e) {
-    try {
-      svg = twemojiSVGs('./' + (code || '').replace(/_/g, '-').toLowerCase() + '.svg');
-    } catch (e2) {
-      console.error("Couldn\'t find svg fro code: " + code);
-    }
-  }
-  return svg;
+  return getFromNotoEmoji(code) || getFromTwemoji(code);
 }
 
 function getCategories() {
-  return (emojis || []).filter(c => c.category).map(c => ({
-    title: c.category,
-    fa: c.fa
+  return categories.map(c => ({
+    title: c.title,
+    fa: c.fa,
+    source: c.source
   }));
 }
 
-function getAllEmojiByCategory(category) {
-  const list = emojis.filter(c => c.category === category).map(c => c.emojis);
+function getSources() {
+  return categories
+    .filter((c, pos, arr) => arr.map(c2 => c2['source']).indexOf(c['source']) === pos)
+    .map(c => (c.source === 'noto-emoji' ? {
+      title: 'Noto Color Emoji',
+      code: c.source,
+      fa: 'fa-google'
+    } : c.source === 'twemoji' ? {
+      title: 'Twemoji',
+      code: c.source,
+      fa: 'fa-twitter'
+    } : {
+      title: c.source,
+      code: c.source,
+      fa: 'fa-question-mark'
+    }));
+}
+
+function getAllEmojiByCategoryAndSource(category, source) {
+  const list = categories.filter(c => c.title === category && c.source === source).map(c => c.emojis);
   return list.length && list[0] || [];
 }
 
-function init() {
-  emojis.forEach(c => c.emojis.forEach(e => {
-    try {
-      e.svg = getEmoji(e.code);
-    } catch (e) {
-      e.invalid = true;
-      e.error = e;
-      console.error(e);
-    }
-  }));
+function loadEmojis() {
+
+  emojis.forEach(category => {
+
+    const notoEmojiCategory = {
+      title: category.category,
+      fa: category.fa,
+      source: 'noto-emoji',
+      emojis: []
+    };
+    const twemojiCategory = {
+      title: category.category,
+      fa: category.fa,
+      source: 'twemoji',
+      emojis: []
+    };
+
+    category.emojis.forEach(emoji => {
+
+      const ne = getFromNotoEmoji(emoji.code);
+      ne && notoEmojiCategory.emojis.push({
+        code: emoji.code,
+        title: emoji.title,
+        svg: ne
+      });
+
+      const tw = getFromTwemoji(emoji.code);
+      tw && twemojiCategory.emojis.push({
+        code: emoji.code,
+        title: emoji.title,
+        svg: tw
+      });
+    });
+
+    notoEmojiCategory.emojis.length && categories.push(notoEmojiCategory);
+    twemojiCategory.emojis.length && categories.push(twemojiCategory);
+  });
 }
 
-init();
+loadEmojis();
 
 function parseEmojione() {
 
@@ -74,12 +146,16 @@ function parseEmojione() {
   }
 
   const json = JSON.stringify(categories);
-  console.debug(json);
+  isDev && console.debug(json);
+  return json;
 }
 
 module.exports = {
   getEmojiByCode: getEmoji,
+  getFromNotoEmojiByCode: getFromNotoEmoji,
+  getFromTwemojiByCode: getFromTwemoji,
+  getSources: getSources,
   getCategories: getCategories,
-  getAllEmojiByCategory: getAllEmojiByCategory,
+  getAllEmojiByCategoryAndSource: getAllEmojiByCategoryAndSource,
   //convert: parseEmojione
 };
